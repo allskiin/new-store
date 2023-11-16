67bf2d92e81c53c755552a37b7ea7af8bb8db913
allskiin <89169894+allskiin@users.noreply.github.com>
2023-11-16 03:58:00 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ btnSearch.addEventListener('click', function(){
     pagSearch.classList.add('active-search');
     hearder.classList.add('active-search');
     menuNav.classList.remove('active-menu-nav');
+    menuNavHeader.classList.remove('active-menu-nav');
 });
 
 closeSearch.addEventListener('click', function(){
@@ -33,4 +34,34 @@ abrirMenuNav.addEventListener('click', function(){
 fecharMenuNav.addEventListener('click', function(){
     menuNav.classList.remove('active-menu-nav');
     menuNavHeader.classList.remove('active-menu-nav');
+});
+
+// modo
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bi-sun';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bi-moon' : 'bi-sun';
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bi-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
 });
